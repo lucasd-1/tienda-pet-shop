2ffc4bee25499490cd9d979327bb9f754be06dcb
Matias Bruchi <matias.bruchi@mulesoft.com>
2020-08-23 11:37:15 -0300

    fixing deps order

--- views/layout/header.php
+++ views/layout/header.php
@@ -8,8 +8,8 @@ ob_start();
     <title>Tienda Pet Shop</title>
     <link rel="stylesheet" href="<?=base_url?>assets/css/styles.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="<?=base_url?>assets/js/functions.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
