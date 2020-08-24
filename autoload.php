8392c3df548cc926d2e1ba777151e1988c5fbde9
Matias Bruchi <matias.bruchi@mulesoft.com>
2020-08-24 13:36:55 -0300

    fixing autoload

--- autoload.php
+++ autoload.php
@@ -18,7 +18,7 @@ if(!function_exists('env')) {
 }
  
 function controllers_autoload($classname){
-    include __DIR__.'/controllers/' . $classname . '.php';
+    require_once __DIR__.'/controllers/' . $classname . '.php';
 }
  
 spl_autoload_register('controllers_autoload');
