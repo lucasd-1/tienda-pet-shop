f81e1c35c2bcd32d110d9682aed21ab16633d026
Matias Bruchi <matiasbruchi@gmail.com>
2020-10-12 18:49:43 -0300

    cambios de estilo (#11)

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,12 +1,55 @@
-</div>
-    </div>
-
-        
-        <!-- FOOTER -->
-
-        <footer id="footer">
-            <p>practica profesional III; <?= date('Y') ?></p>
-        </footer>
         </div>
+        </div>
+
+
+        <!-- FOOTER -->
+        <footer id="footer">
+            <div id = "abajo" class="container">
+                <div id="logito">
+                    <a href="<?=base_url?>"><img src="<?=base_url?>assets/img/gatitolog.png" alt="pet logo"/></a>
+                </div>
+                <div id="datafiscal">
+                    <a href="<?=base_url?>"><img src="<?=base_url?>assets/img/datafiscal.jpg" alt="pet logo"/></a>
+                </div>
+                <div id="mercadopago">
+                    <a href="<?=base_url?>"><img src="<?=base_url?>assets/img/mercadopago.jpg" alt="pet logo"/></a>
+                </div>
+
+                <div id="CATEGORIAS">
+                    <ul>
+                        <li><p><a href="<?=base_url?>">CATEGORIAS</a></p></li>
+                        <li><a href="<?=base_url?>">Perros</a></li>
+                        <li><a href="<?=base_url?>">Gatos</a></li>
+                        <li><a href="<?=base_url?>">Peces</a></li>
+                        <li><a href="<?=base_url?>">Hamsteres</a></li>
+                    </ul>
+                </div>
+                <div id="CARRITO">
+                    <ul>
+                        <li><p><a href="<?=base_url?>">CARRITO</a></p></li>
+                        <li><a href="<?=base_url?>">Mis compras</a></li>
+                        <li><a href="<?=base_url?>">Mi cuenta</a></li>
+                        <li><a href="<?=base_url?>">Terminos y condiciones</a></li>
+                        <li><a href="<?=base_url?>">Politicas de privacidad</a></li>
+                        <li><a href="<?=base_url?>">Politicas de cambios</a></li>
+                    </ul>
+                </div>
+                <div id="CONTACTO">
+                    <ul>
+                        <li><p><a href="<?=base_url?>">CONTACTO</a></p></li>
+                        <li><a href="<?=base_url?>">Nosotros</a></li>
+                        <li><a href="<?=base_url?>">Locales</a></li>
+                        <li><a href="<?=base_url?>">Contacto</a></li>
+                        <li><a href="<?=base_url?>">Noticias</a></li>
+                    </ul>
+                </div>
+                <div id="redes">
+                    <a href="<?=base_url?>"><img src="<?=base_url?>assets/img/face.png" alt="pet logo"/></a>
+                    <a href="<?=base_url?>"><img src="<?=base_url?>assets/img/ig.png" alt="pet logo"/></a>
+                    <a href="<?=base_url?>"><img src="<?=base_url?>assets/img/tw.jpg" alt="pet logo"/></a>
+                    <a href="<?=base_url?>"><img src="<?=base_url?>assets/img/ws.png" alt="pet logo"/></a>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
--- views/layout/header.php
+++ views/layout/header.php
@@ -19,16 +19,24 @@ ob_start();
     <!-- CABECERA -->
     <header id="header">
         <div id="logo">
-            <img src="<?=base_url?>assets/img/lo-firecords.jpg" alt="pet logo" />
-            <a href="<?=base_url?>">
-                Petit shop
-            </a>
+            <img src="<?=base_url?>assets/img/gatitolog.png" alt="pet logo" />
         </div>
+        <!-- Busqueda -->
+        <form class="busqueda" action="<?=base_url?>producto/buscar" method="post">
+            <div class="input-group">
+                <input class="form-control" name="busqueda" type="text" placeholder="Buscar" aria-label="Search">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit">
+                        <i class="glyphicon glyphicon-search"></i>
+                    </button>
+                </div>
+            </div>
+        </form>
     </header>
 
     <!-- MENU -->
     <?php $categorias = Utils::showCategorias(); ?>
-    <nav class="navbar navbar-inverse">
+    <nav class="navbar navbar-inverse" style="margin-bottom: 0px;">
         <div class="container-fluid">
             <div class="navbar-header">
                 <a class="navbar-brand" href="#">Petit Shop</a>
@@ -53,19 +61,43 @@ ob_start();
                     </li>
                 <?php endwhile; ?>
             </ul>
-            <!-- Busqueda -->
-            <form class="navbar-form navbar-right" action="<?=base_url?>producto/buscar" method="post">
-                <div class="input-group">
-                    <input class="form-control" name="busqueda" type="text" placeholder="Buscar" aria-label="Search">
-                    <div class="input-group-btn">
-                        <button class="btn btn-default" type="submit">
-                            <i class="glyphicon glyphicon-search"></i>
-                        </button>
-                    </div>
-                </div>
-
-            </form>
         </div>
     </nav>
+
+    <div class="container" style="padding-left: 0px; padding-right: 18px;">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img src="<?=base_url?>assets/img/promo1.png" alt="Promo1" style="width:100%;">
+                </div>
+
+                <div class="item">
+                    <img src="<?=base_url?>assets/img/promo2.png" alt="Promo2" style="width:100%;">
+                </div>
+
+                <div class="item">
+                    <img src="<?=base_url?>assets/img/promo3.webp" alt="Promo3" style="width:100%;">
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
 
     <div id="content">
